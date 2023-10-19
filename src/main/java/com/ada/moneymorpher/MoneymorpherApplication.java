4c1545bac862376742df4b3e097c10d28e0fdc3d
Nilson Netto <eng.nilsonnetto@gmail.com>
2023-10-19 09:04:00 -0300

    initial commit

---
+++ src/main/java/com/ada/moneymorpher/MoneymorpherApplication.java
@@ -0,0 +1,13 @@
+package com.ada.moneymorpher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymorpherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymorpherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ada/moneymorpher/MoneymorpherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.moneymorpher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymorpherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
