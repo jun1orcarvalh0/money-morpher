b5ff0ae1b8af4ca0ac0a9784fc795cda7e02daf6
Júnior Carvalho <juniorcarvalhodev@gmail.com>
2023-10-25 18:35:37 -0300

    Feat: Implementing currency flow to get exchange rates from external api

--- src/main/java/com/ada/moneymorpher/MoneymorpherApplication.java
+++ src/main/java/com/ada/moneymorpher/MoneymorpherApplication.java
@@ -3,10 +3,12 @@ package com.ada.moneymorpher;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableFeignClients
 @SpringBootApplication
 @RestController()
 public class MoneymorpherApplication {
---
+++ src/main/java/com/ada/moneymorpher/currency/CurrencyRestController.java
@@ -0,0 +1,35 @@
+package com.ada.moneymorpher.currency;
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Currency;
+import java.util.List;
+
+@RestController
+@RequestMapping("/currencies")
+@RequiredArgsConstructor
+public class CurrencyRestController {
+
+    private final CurrencyService service;
+    private final ModelMapper modelMapper;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public CurrencyResponse create(CurrencyRequest request){
+        CurrencyDto response = this.service.create(request);
+        return this.convertResponse(response);
+    }
+
+    @GetMapping()
+    public List<CurrencyDto> listCurrencies (){
+
+        return this.service.listCurrencies();
+    }
+
+    private CurrencyResponse convertResponse(CurrencyDto currency) {
+        return this.modelMapper.map(currency, CurrencyResponse.class);
+    }
+}
